cd356f52ef72834d7d3574d4545681794fe8a255
Charly MZK <charlymrazeck@gmail.com>
2017-12-27 11:45:42 +0100

    .h added

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ all: superbash
 clean:
 	rm *.o superbash
 
+superbash: superbash.o
+	gcc superbash.o -o superbash
 
-superbash: superbash.c 
-	gcc superbash.c -o superbash
+superbash.o: superbash.h superbash.c 
+	gcc -c superbash.c 
--- superbash.c
+++ superbash.c
@@ -2,19 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-#define MAX(X,Y) ((X)>(Y)?(X):(Y))
-
-typedef struct sNode
-{
-	char* command;
-	bool success;
-	char* result;
-	char* separator;
-	char* inputValue;
-	struct sNode* leftChild;
-	struct sNode* rightChild;	
-} Node;
+#include "superbash.h"
 
 Node* create_root(char* v, Node* leftChild, Node* rightChild)
 {
