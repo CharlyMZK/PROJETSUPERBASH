adfb73e3e72b12bd33ff0c12722329ce77ca414b
Baptiste Oberbach <o.baptiste@wanadoo.fr>
2018-02-01 19:39:14 +0000

    ajout de l'enchainage des commandes

--- src/superbash.c
+++ src/superbash.c
@@ -3,9 +3,53 @@
 #include <string.h>
 #include <stdbool.h>
 #include "tree.h"
+#define LSH_RL_BUFSIZE 1024
 
 int main(int argc, char *argv[])
 {
-	create_tree_from_command(argc,argv);
-	return 0;
+	//create_tree_from_command(argc,argv);
+	bash_loop();
+	return EXIT_SUCCESS;
+}
+
+char *read_console_line(void)
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+  while (1) {
+    // Read a character
+    c = getchar();
+    // If we hit EOF, replace it with a null character and return.
+    if (c == EOF || c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+  }
+}
+
+void bash_loop(void)
+{
+  char **command;
+  bool isRunning = true;
+  do {
+    printf("> ");
+    command = read_console_line();
+    isRunning = execute_command(command);
+  } while (isRunning);
+  free(command);
+}
+
+int execute_command(char **command)
+{
+  int i;
+  if (command[0] == NULL) {
+    return false;
+  }else{
+  	return true;
+  }
 }
