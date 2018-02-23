1c3b9e0f5180eb87d6fcd3738611192315e1a2b1
Baptiste Oberbach <o.baptiste@wanadoo.fr>
2018-02-23 11:25:00 +0000

    Refactoring files suite

--- src/commandExecutor.h
+++ src/commandExecutor.h
@@ -1,19 +1,9 @@
-#include "logUtils.h"
+
+#include "commandHandler.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <unistd.h>
-#define LSH_RL_BUFSIZE 1024
-#define OUTPUT_FILEPATH "./tmpOutputFile"
-#define INPUT_FILEPATH "./tmpInputFile"
-
-
-Node* create_tree_from_command(char* command);
-
-bool read_and_exec_tree(Node* treeCommand);
-
-int create_and_execute_tree(char* command);
 
 char** build_command(Node * node);
 
---
+++ src/commandHandler.h
@@ -0,0 +1,8 @@
+#include "logUtils.h"
+#include "stringUtils.h"
+
+Node* create_tree_from_command(char* command);
+
+bool read_and_exec_tree(Node* treeCommand);
+
+int create_and_execute_tree(char* command);
